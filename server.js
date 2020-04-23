6c1b6df49f3a32451116872ea22229381280a6b7
nbrown0000 <brown.nathanjames@gmail.com>
2020-04-23 18:44:12 +1000

    Modify route /searchByTitle Consolidate axios get requests into Promise.all Simplify font-end post request to Movie Recommender API

--- server.js
+++ server.js
@@ -2,24 +2,36 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config({path: __dirname + '/.env'});
 }
 const express = require('express');
-const port = 3000;
+const port = 3300;
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser());
+app.use(bodyParser.json());
 const axios = require('axios');
+
 
 app.get('/', (req,res) => res.send('Hello World!'));
 
-app.post('/search', (req,res) => {
-  const searchTerm = req.body.keywords.split(' ').join('+');
-  const fetchString = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.TMDB_APIKEY}&query=${searchTerm}`;
 
+app.post('/searchByTitle', (req,res) => {
+  let arrayOfFetchUrls = [];
+  const searchTerm = req.body.title.split(' ').join('+');
+  const fetchString = 'https://api.themoviedb.org/3/search/movie?api_key='+
+      process.env.TMDB_APIKEY+'&query='+searchTerm;
   axios.get(fetchString)
-    .then(response => {
-      res.send(response.data.results)
-    })
-    .catch(error => res.send(`Something went wrong: ${error}`))
+  .then(response => {
+    for(let i=1; i<=response.data.total_pages; i++) {
+      arrayOfFetchUrls.push(fetchString+`&page=${i}`)
+    }
+    Promise.all(arrayOfFetchUrls.map(u => axios.get(u)))
+    .then(result => Promise.all(result.map(u => u.data)))
+    .then(result => res.send(result.reduce((acc,val) => {
+      return acc = acc.concat(val.results)
+    }, [] )))
+    .catch(error => console.log(`Error: ${error}`))
+  })
+  .catch(error => console.log(`Error in get request: ${error}`))
 })
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
