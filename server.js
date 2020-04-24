7c3b83dd577c26a83e668c2a6338447a0e3cc709
nbrown0000 <brown.nathanjames@gmail.com>
2020-04-24 18:31:08 +1000

    Prepare to deploy to Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config({path: __dirname + '/.env'});
 }
 const express = require('express');
-const port = 3300;
+const port = 5000;
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -12,14 +12,50 @@ const axios = require('axios');
 app.get('/', (req,res) => res.send('Hello World!'));
 
 
+app.get('/getGenres', (req,res) => {
+  const fetchString = 'https://api.themoviedb.org/3/genre/movie/list?api_key='+
+      process.env.TMDB_APIKEY+'&language=en-US'
+  axios.get(fetchString)
+  .then(response => res.send(response.data))
+})
+
+
 app.post('/searchByTitle', (req,res) => {
   let arrayOfFetchUrls = [];
+  let total_pages = 0;
   const searchTerm = req.body.title.split(' ').join('+');
   const fetchString = 'https://api.themoviedb.org/3/search/movie?api_key='+
       process.env.TMDB_APIKEY+'&query='+searchTerm;
+
   axios.get(fetchString)
   .then(response => {
-    for(let i=1; i<=response.data.total_pages; i++) {
+    if(response.data.total_pages > 5) { total_pages=5; }
+    else { total_pages = response.data.total_pages; }
+    for(let i=1; i<=total_pages; i++) {
+      arrayOfFetchUrls.push(fetchString+`&page=${i}`)
+    }
+    Promise.all(arrayOfFetchUrls.map(u => axios.get(u)))
+    .then(result => Promise.all(result.map(u => u.data)))
+    .then(result => res.send(result.reduce((acc,val) => {
+      return acc = acc.concat(val.results)
+    }, [] )))
+    .catch(error => console.log(`Error: ${error}`))
+  })
+  .catch(error => console.log(`Error in get request: ${error}`))
+})
+
+
+app.post('/searchByGenres', (req,res) => {
+  let arrayOfFetchUrls = [];
+  let total_pages = 0;
+  const fetchString = 'https://api.themoviedb.org/3/discover/movie?api_key='+
+      process.env.TMDB_APIKEY+'&with_genres='+req.body.genre_ids;
+
+  axios.get(fetchString)
+  .then(response => {
+    if(response.data.total_pages > 5) { total_pages=5; }
+    else { total_pages = response.data.total_pages; }
+    for(let i=1; i<=total_pages; i++) {
       arrayOfFetchUrls.push(fetchString+`&page=${i}`)
     }
     Promise.all(arrayOfFetchUrls.map(u => axios.get(u)))
